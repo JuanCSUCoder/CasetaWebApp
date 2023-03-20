15d97fe5c6aa2c81911eab7652396f49a6bc00f0
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-03-20 18:52:09 -0500

    Feat: Base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
+import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: "400",
+  style: "normal",
+  subsets: [ "latin" ]
+})
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className={[styles.main, roboto.className].join(" ")}>
+      <h1>Caseta</h1>
     </main>
-  )
+  );
 }
