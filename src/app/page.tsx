f3e26734c72ae1540a1a3fbb2f639ff7db4dd1b5
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-03-20 21:15:06 -0500

    Feat: Adds icons and descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Roboto } from 'next/font/google'
 import styles from './page.module.css'
 import logo from '@/assets/brand/logo_v1_transparent.png'
 import solanaLogo from '@/assets/solanaLogo.png'
+import { BiKey } from 'react-icons/bi'
+import { HiLightningBolt } from 'react-icons/hi'
 
 import PreSignUp from '@/components/PreSignUp'
 import LandingNav from '@/components/LandingNav'
@@ -23,14 +25,47 @@ export default function Home() {
         <Image src={logo} alt="Caseta Logo" width={300} />
         <LandingNav />
       </HBox>
-      <h2 id="features">
+      <h2>
         A fast, secure and realiable way of making realstate transactions
         globally
       </h2>
       <Block page>
         <HBox spaced>
-          <h1>Hosted on Blockchain</h1>
+          <div id="features">
+            <h1>Hosted on Blockchain</h1>
+            <p className={styles.desc}>
+              Everything is hosted on the blockchain, so all the information is
+              transparent and independent from private or government databases
+            </p>
+          </div>
           <Image src={solanaLogo} alt="Solana Blockchain Logo" width={400} />
+        </HBox>
+      </Block>
+      <Block page>
+        <HBox spaced>
+          <div>
+            <h1>Improved Security with Decentralization</h1>
+            <p className={styles.desc}>
+              Your real-world assets are represented as digital assets that you
+              own. You are in full control of them.
+            </p>
+            <p className={styles.desc}>
+              Assets are never transfered if sale-contract conditions are not met.
+            </p>
+          </div>
+          <BiKey size={200} />
+        </HBox>
+      </Block>
+      <Block page>
+        <HBox spaced>
+          <div>
+            <h1>Just Fast Transactions</h1>
+            <p className={styles.desc}>
+              No need for waiting at government institutions. Just some clicks
+              and your sale is made.
+            </p>
+          </div>
+          <HiLightningBolt size={200} />
         </HBox>
       </Block>
       <Block>
