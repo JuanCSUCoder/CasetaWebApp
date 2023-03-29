4154c863978ecc4cac13ebac702690d2fa4759e0
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-03-29 13:32:21 -0500

    Fix: Spaced Feature for RBox

--- src/components/RBox.tsx
+++ src/components/RBox.tsx
@@ -2,9 +2,5 @@ import { ReactNode } from "react";
 import styles from './RBox.module.css'
 
 export default function RBox({children, spaced}:{children: ReactNode, spaced?: boolean}) {
-    return (
-        <div className={styles.responsive}>
-            {children}
-        </div>
-    )
+    return <div className={[styles.responsive, spaced ? styles.space : ''].join(' ')}>{children}</div>;
 }
